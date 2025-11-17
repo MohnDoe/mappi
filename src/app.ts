301233d979fc466ea90337d23492629cbcdd61a6
mohndoe <bonjour@doe.cool>
2025-11-17 14:46:27 +0100

    feat(middlewares): onError

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import notFound from '@/middlewares/not-found'
 import sets from '@/routes/sets'
+import onError from './middlewares/on-error'
 
 const app = new Hono()
 
 app.notFound(notFound)
+app.onError(onError)
 
 app.get('/', (c) => {
   return c.json({
---
+++ src/middlewares/on-error.ts
@@ -0,0 +1,17 @@
+import type { ErrorHandler } from 'hono'
+import type { ContentfulStatusCode } from 'hono/utils/http-status'
+
+const onError: ErrorHandler = (err, c) => {
+  const currentStatus = 'status' in err ? err.status : c.newResponse(null).status
+
+  const statusCode = (currentStatus !== 200 ? currentStatus : 500) as ContentfulStatusCode
+
+  // eslint-disable-next-line node/prefer-global/process
+  const env = c.env.NODE_ENV || process.env.NODE_ENV
+  return c.json({
+    message: err.message,
+    stack: env === 'production' ? undefined : err.stack,
+  }, statusCode)
+}
+
+export default onError
