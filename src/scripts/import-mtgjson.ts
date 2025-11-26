8766253f819b13ec13093b10cb81003ab3c823ec
mohndoe <bonjour@doe.cool>
2025-11-26 16:37:37 +0100

    feat: download "all sets" zip file from MTGJSON and unzip it

---
+++ src/scripts/import-mtgjson.ts
@@ -0,0 +1,133 @@
+import type { Meta } from '@/models/mtgjson.types'
+import { mkdir } from 'node:fs/promises'
+import { createZipReader } from '@holmlibs/unzip'
+import { semver } from 'bun'
+import pino from 'pino'
+import PinoPretty from 'pino-pretty'
+
+const MTGJSON_URL = 'https://mtgjson.com/api/v5'
+const MTGJSON_ZIP_FILE_URL = `${MTGJSON_URL}/AllSetFiles.zip`
+const MTGJSON_META_FILE_URL = `${MTGJSON_URL}/Meta.json`
+
+const CACHE_DIR = './tmp/mtgjson-data'
+const CACHE_EXTRACT_DIR = `${CACHE_DIR}/SetsFiles`
+const CACHE_ZIP_FILE = `${CACHE_DIR}/AllSetFiles.zip`
+
+const LOCAL_MTGJSON_DATA_DIR = `./data/MTGJSON`
+const LOCAL_MTGJSON_VERSION_FILE = `${LOCAL_MTGJSON_DATA_DIR}/version.json`
+
+const logger = pino({
+  level: 'debug',
+}, PinoPretty())
+
+async function main() {
+  const needUpdate = await shouldUpdate()
+
+  if (needUpdate) {
+    const remoteMeta = await getRemoteMTGJSONVersion()
+    await getAllSetsZip()
+
+    await unzipAllSetFiles()
+
+    await saveLocalVersion(remoteMeta)
+  }
+  else {
+    logger.info('No new version found, skipping download')
+  }
+}
+
+async function unzipAllSetFiles() {
+  const reader = createZipReader(CACHE_ZIP_FILE)
+
+  await reader.extractAll(CACHE_EXTRACT_DIR, (current, total) => {
+    logger.info(`Extracting: ${current}/${total} files`)
+  })
+}
+
+async function downloadZipFromRemote() {
+  logger.info(`Downloading ZIP from MTGJSON`)
+  const url = MTGJSON_ZIP_FILE_URL
+
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`Failed to download sealed data from ${url}`)
+  }
+
+  logger.info(`Download. Success!`)
+
+  return response.arrayBuffer()
+}
+
+async function getAllSetsZip() {
+  const cachedFile = Bun.file(CACHE_ZIP_FILE)
+  const exists = await cachedFile.exists()
+
+  if (exists) {
+    logger.info(`Loading sets from cached ZIP`)
+    return
+  }
+
+  const content = await downloadZipFromRemote()
+
+  await mkdir(CACHE_DIR, { recursive: true })
+
+  await Bun.write(CACHE_ZIP_FILE, content)
+  logger.info(`Saved sets ZIP to cache`)
+}
+
+async function getRemoteMTGJSONVersion(): Promise<Meta> {
+  const url = MTGJSON_META_FILE_URL
+
+  const response = await fetch(url)
+
+  if (!response.ok) {
+    throw new Error(`Failed fetching current MTJJSON version (${url})`)
+  }
+
+  const metaFileContent = await response.json() as {
+    data: Meta
+  }
+
+  return (metaFileContent.data) as Meta
+}
+
+async function getLocalMTGJSONVersion(): Promise<Meta | null> {
+  const versionFile = Bun.file(LOCAL_MTGJSON_VERSION_FILE)
+  const exists = await versionFile.exists()
+
+  if (!exists) {
+    return null
+  }
+
+  return (await versionFile.json() as Meta)
+}
+
+async function shouldUpdate() {
+  const localVersion = await getLocalMTGJSONVersion()
+
+  if (!localVersion)
+    return true
+
+  logger.debug(`Local MTGJSON version ${localVersion.version}::${localVersion.date}`)
+  const remoteVersion = await getRemoteMTGJSONVersion()
+
+  if (!remoteVersion)
+    return true
+
+  logger.debug(`Remote MTGJSON version ${remoteVersion.version}::${remoteVersion.date}`)
+
+  try {
+    return semver.order(localVersion.version, remoteVersion.version) === 1
+  }
+  catch (error) {
+    logger.error(`Error while checking MTGJSON version : ${error}`)
+    return true
+  }
+}
+
+async function saveLocalVersion(meta: Meta) {
+  await mkdir(LOCAL_MTGJSON_DATA_DIR, { recursive: true })
+  await Bun.write(LOCAL_MTGJSON_VERSION_FILE, JSON.stringify(meta))
+}
+
+main()
